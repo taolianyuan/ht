af00da50c593b0b9e345673717fbad0aefa176b4
liantao <30997528+taolianyuan@users.noreply.github.com>
2018-08-15 15:40:39 +0800

    Add files via upload

---
+++ lingxiaomanbu/src/components/page/EditorPage.vue
@@ -0,0 +1,82 @@
+<template>
+    <div >
+        <v-pageTitle vtitle="EditorPage"></v-pageTitle>
+
+
+        <!-- use with components - bidirectional data binding（双向数据绑定） -->
+        <quill-editor ref="myTextEditor"
+                    v-model="content"
+                    :config="editorOption"
+                    @blur="onEditorBlur($event)"
+                    @focus="onEditorFocus($event)"
+                    @ready="onEditorReady($event)">
+        </quill-editor>
+
+         <div class="html ql-editor" v-html="content"></div>
+
+    </div>
+
+</template>
+
+<script>
+    import vPageTitle from '../common/pageTitle.vue';
+    import { quillEditor } from 'vue-quill-editor';
+    
+
+    
+   
+    export default {
+        components:{
+            vPageTitle,quillEditor
+        },
+        data(){
+            return{
+                content:'<h2>您好</h2><h3>欢迎来到管理后台</h3>',
+                editorOption: {}
+            }
+        },
+        methods: {  
+             onEditorBlur(editor) {
+                    console.log('editor blur!', editor)
+                },
+                onEditorFocus(editor) {
+                    console.log('editor focus!', editor)
+                },
+                onEditorReady(editor) {
+                    console.log('editor ready!', editor)
+                }    
+        },
+        computed: {
+            editor() {
+                return this.$refs.myTextEditor.quillEditor;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .el-col{
+        margin-bottom:16px;
+    }
+    .material-icons{
+        font-size:80px;
+        color:#ddd;
+    }
+   
+    .ql-container .ql-editor {
+        min-height: 20em;
+        padding-bottom: 1em;
+        max-height: 25em;
+        
+    }
+    .html {
+        height: 9em;
+        overflow-y: auto;
+        border: 1px solid #ccc;
+        border-top: none;
+        resize: vertical;
+        background-color:#fff;
+    }
+ 
+    
+</style>
---
+++ lingxiaomanbu/src/components/tables/fixedHeaderTable.vue
@@ -0,0 +1,70 @@
+<template>
+   <el-table :data="tableData" style="width: 100%" border stripe height="250">
+      <el-table-column prop="id"  label="ID"  width="80"></el-table-column>
+      <el-table-column prop="userName" label="NAME" width="180"></el-table-column>
+      <el-table-column prop="age" label="AGE" width="80"> </el-table-column>
+      <el-table-column prop="ability" label="ABILITY"> </el-table-column>
+      <el-table-column prop="value" label="VALUE" align="right"> </el-table-column>
+    </el-table>
+</template>
+
+<script>
+    export default {
+      data() {
+        return {
+          tableData: [{
+            id:1,
+            userName: '小王',
+            age: '30',
+            ability: 'Web,HTML5,PHP',
+            value:'80%'
+          }, {
+            id:2,
+            userName: '小李',
+            age: '28',
+            ability: 'Web,PHP',
+            value:'70%'
+          }, {
+            id:3,
+            userName: '小孙',
+            age: '30',
+            ability: 'PHP,MySql',
+            value:'60%'
+          }, {
+            id:4,
+            userName: '小何',
+            age: '30',
+            ability: 'PHP,MySql',
+            value:'60%'
+          },{
+            id:5,
+            userName: '小孙',
+            age: '30',
+            ability: 'PHP,MySql',
+            value:'60%'
+          },{
+            id:6,
+            userName: '小孙',
+            age: '30',
+            ability: 'PHP,MySql',
+            value:'60%'
+          },{
+            id:7,
+            userName: '晓明',
+            age: '30',
+            ability: 'PHP,MySql',
+            value:'60%'
+          },{
+            id:8,
+            userName: '晓明',
+            age: '35',
+            ability: 'Web,HTML5,PHP,DB',
+            value:'80%'
+          }]
+        }
+      }
+    }
+</script>
+<style scoped>
+    
+</style>
